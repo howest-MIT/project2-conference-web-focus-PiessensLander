416d8b21192b53f5f5b32826eabc4d8d08be7e44
Lander <lander.piessens@student.howest.be>
2021-05-30 18:48:08 +0200

    index additions

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 		</div>
 	</div>
 
-	<div class="owl-carousel c-carousel">
+	<div class="owl-carousel c-carousel mb-5">
 
 		<div class="c-carousel__item">
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
@@ -45,6 +45,99 @@
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
 		</div>
 	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="c-intro d-flex justify-content-between align-items-center">
+				<div class="c-intro__halls d-flex flex-column align-items-center">
+					<h4>5 halls</h4>
+					<a href="#" class="c-caption">view all</a>
+				</div>
+				<div class="c-intro__about d-flex flex-column align-items-center justify-content-center">
+					<h2 class="mb-4">About Multi-Mania</h2>
+					<p class="text-center mb-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+					<a href="#" class="c-intro__btn c-btn c-btn--pink py-2 px-5 w-auto">Buy tickets</a>
+				</div>
+				<div class="c-intro__speakers d-flex flex-column align-items-center">
+					<h4>28 speakers</h4>
+					<a href="#" class="c-caption">view all</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="c-speakers">
+		<div class="container">
+			<h2 class="text-white text-center mb-4">Some of our speakers</h2>
+			<div class="row d-flex mb-5 justify-content-center">
+				<div class="col-sm-6 col-md-4 col-xl-3 mt-3 align-items-stretch">
+					<div class="c-card p-4">
+						<div class="c-card__header mb-3">
+							<h3>Aral Balkan</h3>
+						</div>
+						<div class="c-card__image mb-3">
+							<img src="https://www.troyhunt.com/content/images/2016/12/P1220073.jpg" alt="Speaker">
+						</div>
+						<div class="c-card__description mb-3 d-flex">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda temporibus esse ipsam! Veniam ea reprehenderit amet animi magni fugit odio placeat sapiente facilis quam, autem reiciendis itaque culpa aliquam tempora.</^p>
+						</div>
+						<div class="c-card__footer d-flex flex-wrap justify-content-between">
+							<a class="c-btn c-btn--outline py-2 px-5 w-auto">
+								Like
+							</a>
+							<a class="c-btn c-btn--pink py-2 px-5 w-auto">
+								More info
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-xl-3 mt-3">
+					<div class="c-card p-4">
+						<div class="c-card__header mb-3">
+							<h3>Aral Balkan</h3>
+						</div>
+						<div class="c-card__image mb-3">
+							<img src="https://www.troyhunt.com/content/images/2016/12/P1220073.jpg" alt="Speaker">
+						</div>
+						<div class="c-card__description mb-3">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus odio eligendi velit unde eos praesentium, illo harum placeat eaque voluptatem.</p>
+						</div>
+						<div class="c-card__footer d-flex flex-wrap justify-content-between">
+							<a class="c-btn c-btn--outline py-2 px-5 w-auto">
+								Like
+							</a>
+							<a class="c-btn c-btn--pink py-2 px-5 w-auto">
+								More info
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-xl-3 mt-3">
+					<div class="c-card p-4">
+						<div class="c-card__header mb-3">
+							<h3>Aral Balkan</h3>
+						</div>
+						<div class="c-card__image mb-3">
+							<img src="https://www.troyhunt.com/content/images/2016/12/P1220073.jpg" alt="Speaker">
+						</div>
+						<div class="c-card__description mb-3">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium nihil possimus quo consequuntur ipsam molestias iure libero placeat veritatis. Neque autem unde veritatis tempora reprehenderit eveniet nesciunt fuga maxime? Fugit voluptatum, incidunt modi culpa maxime amet numquam assumenda odio sunt illo asperiores facere aut cupiditate, a quos quaerat dolor!</p>
+						</div>
+						<div class="c-card__footer d-flex flex-wrap justify-content-between">
+							<a class="c-btn c-btn--outline py-2 px-5 w-auto">
+								Like
+							</a>
+							<a class="c-btn c-btn--pink py-2 px-5 w-auto">
+								More info
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="#" class="c-btn c-btn--outline py-2 px-5 w-auto">View more</a>
+		</div>
+	</div>
+
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
