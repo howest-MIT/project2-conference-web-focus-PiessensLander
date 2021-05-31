cbf58ab0a6ba39e9f1348c2d007a82326274a6b2
Lander <lander.piessens@student.howest.be>
2021-05-31 12:19:25 +0200

    update carousel img

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php include "src/helper/header.php" ?>
 
 <body>
-	<div class="bg" style="height: 75vh"></div>
+	<div class="bg" style="height: 95vh"></div>
 
 	<div class="container">
 		<div class="row py-4">
@@ -24,25 +24,25 @@
 		</div>
 	</div>
 
-	<div class="owl-carousel c-carousel mb-5">
+	<div class="owl-carousel c-carousel mb-5 d-flex align-items-center">
 
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_1.jpg" alt="Conference" class=" c-img">
 		</div>
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_2.jpg" alt="Conference" class=" c-img">
 		</div>
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_3.jpg" alt="Conference" class=" c-img">
 		</div>
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_4.jpg" alt="Conference" class=" c-img">
 		</div>
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_5.jpg" alt="Conference" class=" c-img">
 		</div>
 		<div class="c-carousel__item">
-			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcNQid_kIRW2Loj-xw5hoOmGuHxjRcY6sNOW15b2GQViLf2UJN5YnTT1_iiMcEIOOB-I8&usqp=CAU" alt="" class=" c-img">
+			<img src="img/carousel_6.jpg" alt="Conference" class=" c-img">
 		</div>
 	</div>
 
@@ -54,7 +54,7 @@
 						<path style="fill: #c54893;" d="M18 18V7.132l-8-4.8-8 4.8V18h4v-2.75a4 4 0 1 1 8 0V18h4zm-6 2v-4.75a2 2 0 1 0-4 0V20H2a2 2 0 0 1-2-2V7.132a2 2 0 0 1 .971-1.715l8-4.8a2 2 0 0 1 2.058 0l8 4.8A2 2 0 0 1 20 7.132V18a2 2 0 0 1-2 2h-6z"></path>
 					</svg>
 					<h4>5 halls</h4>
-					<a href="#" class="c-caption">view all</a>
+					<a href="#" class="text-black">view all</a>
 				</div>
 				<div class="c-intro__about d-flex flex-column align-items-center justify-content-center">
 					<h2 class="mb-4">About Multi-Mania</h2>
@@ -66,7 +66,7 @@
 						<path style="fill: #c54893;" d="M3.534 11.07a1 1 0 1 1 .733 1.86A3.579 3.579 0 0 0 2 16.26V18a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1.647a3.658 3.658 0 0 0-2.356-3.419 1 1 0 1 1 .712-1.868A5.658 5.658 0 0 1 14 16.353V18a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3v-1.74a5.579 5.579 0 0 1 3.534-5.19zM7 1a4 4 0 0 1 4 4v2a4 4 0 1 1-8 0V5a4 4 0 0 1 4-4zm0 2a2 2 0 0 0-2 2v2a2 2 0 1 0 4 0V5a2 2 0 0 0-2-2zm9 17a1 1 0 0 1 0-2h1a1 1 0 0 0 1-1v-1.838a3.387 3.387 0 0 0-2.316-3.213 1 1 0 1 1 .632-1.898A5.387 5.387 0 0 1 20 15.162V17a3 3 0 0 1-3 3h-1zM13 2a1 1 0 0 1 0-2 4 4 0 0 1 4 4v2a4 4 0 0 1-4 4 1 1 0 0 1 0-2 2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z"></path>
 					</svg>
 					<h4>28 speakers</h4>
-					<a href="#" class="c-caption">view all</a>
+					<a href="#" class="text-black">view all</a>
 				</div>
 			</div>
 		</div>
